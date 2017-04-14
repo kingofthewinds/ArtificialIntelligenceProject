452a31c92a857e2150c55f5eefac236c7a4468ae
Lucas Lefèvre <lucas.lefevre@hotmail.com>
2017-04-14 10:43:50 +0200

    Genetic algorithm structure

---
+++ src/sample/GeneticAlgorithm.java
@@ -0,0 +1,35 @@
+package sample;
+
+import java.util.List;
+
+/**
+ * Created by lucas on 14/04/17.
+ */
+public class GeneticAlgorithm {
+
+    private List<Network> population;
+
+    public GeneticAlgorithm() {
+
+    }
+
+    public void breedPopulation() {
+
+    }
+
+    public void generateNewPopulation() {
+
+    }
+
+    private void createNewGenome() {
+        
+    }
+
+    private void crossBreed() {
+
+    }
+
+    private void mutate() {
+
+    }
+}
